99ee8848cb066cfac8838b725abda9c370ebee04
Ken Lee <hershey@hershey.lan>
2021-02-02 23:22:24 -0600

    update docs and scripts

---
+++ infra/local/01-create-cluster.sh
@@ -0,0 +1,15 @@
+rm -rf tmp/vol
+mkdir -p tmp/vol
+
+k3d cluster delete istio-kn-starter
+k3d cluster create istio-kn-starter --servers 1 --agents 3 --volume $(pwd)/tmp/vol:/tmp/vol --k3s-server-arg --disable=traefik  --wait
+
+kubectx k3d-istio-kn-starter
+
+node_count=$(kubectl get nodes | grep -w 'Ready' | wc -l)
+until [ $node_count -eq "4" ]; do
+    echo "waiting for node count"
+    node_count=$(kubectl get nodes | grep -w 'Ready' | wc -l)
+done
+
+kubectl get nodes
