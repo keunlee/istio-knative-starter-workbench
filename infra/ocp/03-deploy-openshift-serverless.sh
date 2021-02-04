fddc63c45c9a3e6c59c1c8efa0296bddc71e5ff6
keunlee <kelee@redhat.com>
2021-02-04 17:03:56 -0600

    update docs and scripts

--- infra/ocp/03-deploy-openshift-serverless.sh
+++ infra/ocp/03-deploy-openshift-serverless.sh
@@ -1,17 +1,42 @@
+# create knative-serving namespace
 oc new-project knative-serving
 
+# deploy knative service
 oc apply -f infra/ocp/operators/crd-instances/serverless/knative-serving.yaml
 
 sleep 120
 
-# NAME                                      READY   UP-TO-DATE   AVAILABLE   AGE
-# deployment.apps/activator                 2/2     2            2           5m27s
-# deployment.apps/autoscaler                1/1     1            1           5m26s
-# deployment.apps/autoscaler-hpa            2/2     2            2           5m22s
-# deployment.apps/controller                2/2     2            2           5m26s
-# deployment.apps/kn-cli-2gls9-deployment   1/1     1            1           4m46s
-# deployment.apps/webhook                   1/1     1            1           5m25s
+# wait for knative deployments to deploy
+kubectl -n knative-serving wait --for=condition=Available  --timeout=360s deployment.apps/activator
+kubectl -n knative-serving wait --for=condition=Available  --timeout=360s deployment.apps/autoscaler
+kubectl -n knative-serving wait --for=condition=Available  --timeout=360s deployment.apps/autoscaler-hpa     
+kubectl -n knative-serving wait --for=condition=Available  --timeout=360s deployment.apps/controller
+kubectl -n knative-serving wait --for=condition=Available  --timeout=360s deployment.apps/webhook
 
 # Create a network policy that permits traffic flow from Knative system Pods to Knative service
 oc label namespace knative-serving serving.knative.openshift.io/system-namespace=true
 oc label namespace knative-serving-ingress serving.knative.openshift.io/system-namespace=true
+
+# allows ksvc networking on a specific namespace
+function enable_ksvc_on_namespace() {
+cat <<EOF | oc apply -f -
+apiVersion: networking.k8s.io/v1
+kind: NetworkPolicy
+metadata:
+  name: allow-from-serving-system-namespace
+  namespace: $1
+spec:
+  ingress:
+  - from:
+    - namespaceSelector:
+        matchLabels:
+          serving.knative.openshift.io/system-namespace: "true"
+  podSelector: {}
+  policyTypes:
+  - Ingress
+EOF
+}
+
+# enable ksvc networking on a specific namespace
+enable_ksvc_on_namespace "knative-sandbox"
+
--- infra/ocp/99-cleanup.sh
+++ infra/ocp/99-cleanup.sh
@@ -1,18 +1,29 @@
-# delete service mesh crd instances and projects
+function wait_for_zero_resource_count() {
+    project_resource_count=$(oc get all -o json | jq -r '.items' | jq length)
+    until [ $project_resource_count -eq "0" ]; do
+        echo "waiting for resources to be purged"
+        project_resource_count=$(oc get all -o json | jq -r '.items' | jq length)
+        sleep 5
+    done    
+}
+
+# delete knative serving instances, resources, and projects
+oc project knative-serving
+oc delete po -l app=storage-version-migration
+oc delete -f infra/ocp/operators/crd-instances/serverless/knative-serving.yaml
+
+wait_for_zero_resource_count
+
+# delete service mesh crd instances, resources, and projects
 oc project istio-system
 oc delete -f infra/ocp/operators/crd-instances/service-mesh/service-mesh-member-roll.yaml
 oc delete -f infra/ocp/operators/crd-instances/service-mesh/service-mesh-control-plane.yaml
 
-project_resource_count=$(oc get all -o json | jq -r '.items' | jq length)
-
-until [ $project_resource_count -eq "0" ]; do
-    echo "waiting for resources to be purged"
-    project_resource_count=$(oc get all -o json | jq -r '.items' | jq length)
-    sleep 5
-done
+wait_for_zero_resource_count
 
 oc delete project bookinfo
 oc delete project knative-sandbox
+oc delete project knative-serving
 oc delete project istio-system
 
 # remove operators
