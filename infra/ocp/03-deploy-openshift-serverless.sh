27d8f0fb2cd54bf6a5c2bcb157c1c53c5646484d
Ken Lee <hershey@hershey.lan>
2021-02-04 20:57:37 -0600

    update docs and scripts

--- infra/ocp/03-deploy-openshift-serverless.sh
+++ infra/ocp/03-deploy-openshift-serverless.sh
@@ -1,23 +1,4 @@
-# create knative-serving namespace
-oc new-project knative-serving
-
-# deploy knative service
-oc apply -f infra/ocp/operators/crd-instances/serverless/knative-serving.yaml
-
-sleep 120
-
-# wait for knative deployments to deploy
-kubectl -n knative-serving wait --for=condition=Available  --timeout=360s deployment.apps/activator
-kubectl -n knative-serving wait --for=condition=Available  --timeout=360s deployment.apps/autoscaler
-kubectl -n knative-serving wait --for=condition=Available  --timeout=360s deployment.apps/autoscaler-hpa     
-kubectl -n knative-serving wait --for=condition=Available  --timeout=360s deployment.apps/controller
-kubectl -n knative-serving wait --for=condition=Available  --timeout=360s deployment.apps/webhook
-
-# Create a network policy that permits traffic flow from Knative system Pods to Knative service
-oc label namespace knative-serving serving.knative.openshift.io/system-namespace=true
-oc label namespace knative-serving-ingress serving.knative.openshift.io/system-namespace=true
-
-# allows ksvc networking on a specific namespace
+# this function allows ksvc networking on a specific namespace
 function enable_ksvc_on_namespace() {
 cat <<EOF | oc apply -f -
 apiVersion: networking.k8s.io/v1
@@ -36,6 +17,29 @@ spec:
   - Ingress
 EOF
 }
+
+# create knative-serving namespace
+oc new-project knative-serving
+
+# wait a minute for the project to initialize
+sleep 60
+
+# deploy knative service
+oc apply -f infra/ocp/operators/crd-instances/serverless/knative-serving.yaml
+
+# wait a few
+sleep 120
+
+# wait for knative deployments to deploy
+kubectl -n knative-serving wait --for=condition=Available  --timeout=360s deployment.apps/activator
+kubectl -n knative-serving wait --for=condition=Available  --timeout=360s deployment.apps/autoscaler
+kubectl -n knative-serving wait --for=condition=Available  --timeout=360s deployment.apps/autoscaler-hpa     
+kubectl -n knative-serving wait --for=condition=Available  --timeout=360s deployment.apps/controller
+kubectl -n knative-serving wait --for=condition=Available  --timeout=360s deployment.apps/webhook
+
+# Create a network policy that permits traffic flow from Knative system Pods to Knative service
+oc label namespace knative-serving serving.knative.openshift.io/system-namespace=true
+oc label namespace knative-serving-ingress serving.knative.openshift.io/system-namespace=true
 
 # enable ksvc networking on a specific namespace
 enable_ksvc_on_namespace "knative-sandbox"
